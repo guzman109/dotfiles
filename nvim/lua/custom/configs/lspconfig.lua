74aa485eebebc99f7ab57f13b84fb0abcd787ab3
Carlos Guzman <42706936+guzman109@users.noreply.github.com>
2023-11-29 18:24:24 -0500

    Add files via upload

--- nvim/lua/custom/configs/lspconfig.lua
+++ nvim/lua/custom/configs/lspconfig.lua
@@ -1,9 +1,8 @@
 local config = require("plugins.configs.lspconfig")
 local on_attach = config.on_attach
 local capabilities = config.capabilities
-
 local lspconfig = require( "lspconfig" )
-local servers = { "html", "cssls", "clangd", "pyright"}
+
 
 -- Without the loop, you would have to manually set up each LSP  
 lspconfig.html.setup {
@@ -20,4 +19,29 @@ lspconfig.pyright.setup {
   on_attach = on_attach,
   capabilities = capabilities,
   filetypes = { "python" },
+}
+
+lspconfig.marksman.setup {
+  on_attach = on_attach,
+  capabilities = capabilities,
+  filetypes = { "markdown" },
+}
+
+lspconfig.clangd.setup {
+  on_attach = function(client, bufnr)
+    client.server_capabilities.signatureHelpProvider = false
+    on_attach(client, bufnr)
+  end,
+  capabilities = capabilities,
+  filetypes = { "cpp" }
+}
+
+lspconfig.tsserver.setup {
+  on_attach = on_attach,
+  capabilities = capabilities,
+  init_options = {
+    preferences = {
+      disableSuggestions = true,
+    }
+  }
 }
